edb2be482d6ca7cde3bffcd5debc80bf8f9957f0
root <root@DESKTOP-T7KLEOV.localdomain>
2022-07-24 01:28:47 +0300

    until 12.11 completed

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const redis = require('../redis')
 
 const configs = require('../util/config')
 
 let visits = 0
+
+const initRedis = async () => {
+  const redisDb = await redis.getAsync("added_todos")
+  if (!redisDb) {
+    await redis.setAsync("added_todos", 0)
+  }
+}
+initRedis();
 
 /* GET index data. */
 router.get('/', async (req, res) => {
@@ -14,5 +23,14 @@ router.get('/', async (req, res) => {
     visits
   });
 });
+
+router.get('/statistics', async (req, res) => {
+  const getRedisDb = await redis.getAsync("added_todos")
+  const todoCounter = Number(getRedisDb) ? Number(getRedisDb) : 0
+  const redisDb = JSON.stringify({
+    "added_todos": todoCounter
+  })
+  res.send(redisDb)
+})
 
 module.exports = router;
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
+const redis = require('../redis');
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -15,6 +16,9 @@ router.post('/', async (req, res) => {
     done: false
   })
   res.send(todo);
+  const getRedisDb = await redis.getAsync("added_todos")
+  const todoCounter = Number(getRedisDb) ? Number(getRedisDb) : 0
+  await redis.setAsync("added_todos", (todoCounter + 1))
 });
 
 const singleRouter = express.Router();
